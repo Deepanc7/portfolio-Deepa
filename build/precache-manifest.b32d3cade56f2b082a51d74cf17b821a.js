612fb2f38cf027cc8b74a04fc99398ae32f548be
Deepanc7 <deepanc2001@gmail.com>
2022-09-20 21:49:26 +0530

    first commit

---
+++ build/precache-manifest.b32d3cade56f2b082a51d74cf17b821a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cd7740984be92a3d59a",
+    "url": "/static/css/main.20a9526e.chunk.css"
+  },
+  {
+    "revision": "5cd7740984be92a3d59a",
+    "url": "/static/js/main.5cd77409.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "83844bbf42714eb9cfc0",
+    "url": "/static/js/2.83844bbf.chunk.js"
+  },
+  {
+    "revision": "36777d478a7e515f8b01",
+    "url": "/static/js/runtime~main.36777d47.js"
+  },
+  {
+    "revision": "56caa67c917a3a848293c310361ce039",
+    "url": "/index.html"
+  }
+];
